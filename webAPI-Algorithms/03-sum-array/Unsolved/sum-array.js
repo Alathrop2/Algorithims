ebc56bab131ca44a7f139c8cf83068566d3c89c6
Alathrop2 <98856957+Alathrop2@users.noreply.github.com>
2022-09-15 22:06:30 -0600

    completed some of the algoritihms

--- webAPI-Algorithms/03-sum-array/Unsolved/sum-array.js
+++ webAPI-Algorithms/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,9 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function (arr) {
+  let returnNum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    returnNum += arr[i];
+  }
+  return returnNum;
+};
